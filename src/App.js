bb18ff4208110f9bc2aaacc2b061b630bb30d34f
Muhammad Afzal Khan <mafzalkhan1997@gmail.com>
2019-04-28 04:37:43 +0500

    update

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import classNames from "classnames";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
 import TextField from "@material-ui/core/TextField";
-
+import Slide from "@material-ui/core/Slide";
 import IconButton from "@material-ui/core/IconButton";
 import Input from "@material-ui/core/Input";
 import InputLabel from "@material-ui/core/InputLabel";
@@ -14,8 +14,9 @@ import FormHelperText from "@material-ui/core/FormHelperText";
 import FormControl from "@material-ui/core/FormControl";
 // import TextField from "@material-ui/core/TextField";
 import MenuItem from "@material-ui/core/MenuItem";
-import Visibility from "@material-ui/icons/Visibility";
+import ArrowRight from "@material-ui/icons/ArrowRightAlt";
 import VisibilityOff from "@material-ui/icons/VisibilityOff";
+import Button from "@material-ui/core/Button";
 
 import "./App.css";
 // import "typeface-roboto";
@@ -24,7 +25,7 @@ const styles = theme => ({
   root: {
     display: "flex",
     backgroundColor: "green",
-    padding: `0 20px`
+    padding: `0 15px`
   },
   paper: {
     ...theme.mixins.gutters(),
@@ -52,6 +53,33 @@ const styles = theme => ({
     paddingTop: 20,
     paddingBottom: 15
     // width: 350
+  },
+
+  bootstrapRoot: {
+    background: "#a64bf4",
+    background: "linear-gradient(45deg, #fc00ff ,#00dbde)",
+    boxShadow: "none",
+    textTransform: "none",
+    fontSize: 16,
+    borderRadius: 30,
+    // padding: "6px 12px",
+    lineHeight: 2.5,
+    border: "none",
+    width: "100%",
+    // transition: right 0.2s;
+    "&:hover": {
+      // background: "linear-gradient(45deg, #00dbde, #fc00ff )",
+      // transition: "0.2s"
+    },
+    "&:active": {
+      boxShadow: "none"
+      // backgroundColor: "#0062cc",
+      // borderColor: "#005cbf"
+    },
+    "&:focus": {
+      boxShadow: "none"
+      // boxShadow: "0 0 0 0.2rem rgba(0,123,255,.5)"
+    }
   }
 });
 
@@ -68,12 +96,26 @@ const budget = ["Select Budget", "1500 $", "2000 $", "2500 $"];
 class App extends Component {
   state = {
     service: services[0],
-    budget: budget[0]
+    budget: budget[0],
+    marginLeft: 7
   };
 
   handleChange = name => event => {
     // console.log("name", name, "event", event);
     this.setState({ [name]: event.target.value });
+  };
+
+  handleMouse = value => event => {
+    if (value == "enter") {
+      this.setState({
+        marginLeft: 18
+      });
+    }
+    if (value == "out") {
+      this.setState({
+        marginLeft: 7
+      });
+    }
   };
 
   render() {
@@ -90,8 +132,8 @@ class App extends Component {
           >
             Say Hello!
           </Typography>
-          <div style={{ margin: "6%" }}>
-            <FormControl fullWidth style={{ marginTop: 20 }}>
+          <div className="innerDiv">
+            <FormControl fullWidth style={{ marginTop: 25 }}>
               <InputLabel
                 className="fields"
                 style={{ marginLeft: 8, fontSize: 17 }}
@@ -221,6 +263,23 @@ class App extends Component {
                 margin="normal"
               />
             </FormControl>
+            <Button
+              onMouseEnter={this.handleMouse("enter")}
+              onMouseLeave={this.handleMouse("out")}
+              variant="contained"
+              color="primary"
+              // disableRipple
+              style={{ marginTop: 30, marginBottom: 20 }}
+              className={classNames("fields", classes.bootstrapRoot, "button")}
+            >
+              Submit
+              <ArrowRight
+                style={{
+                  marginLeft: this.state.marginLeft,
+                  transition: "0.28s"
+                }}
+              />
+            </Button>
           </div>
         </Paper>
       </div>
